86a27d1aed8fe4b601c64b3ef9927b1d5f9b5aed
Ghantfasia <aliceghanimi@hotmail.com>
2025-03-12 09:32:08 +0100

    feat:client

--- application/src/Controller/PdfController.php
+++ application/src/Controller/PdfController.php
@@ -4,6 +4,8 @@
 
 namespace App\Controller;
 
+use DateTimeImmutable;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +14,6 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use App\Entity\File;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Routing\Annotation\Route;
 
 class PdfController extends AbstractController
 {
@@ -55,7 +56,7 @@ class PdfController extends AbstractController
                     // Créer une nouvelle entité File et la remplir
                     $file = new File();
                     $file->setName($originalFileName);  // Enregistrer le nom du fichier téléchargé
-                    $file->setCreatedAt(new \DateTimeImmutable());
+                    $file->setCreatedAt(new DateTimeImmutable());
 
                     // Sauvegarder l'entité File dans la base de données
                     $this->em->persist($file);
@@ -63,7 +64,7 @@ class PdfController extends AbstractController
 
                     // Retourner la réponse PDF après avoir persisté l'entité File
                     return $pdfResponse;
-                } catch (\RuntimeException $e) {
+                } catch (RuntimeException $e) {
                     return new Response($e->getMessage(), 500);
                 }
             }
