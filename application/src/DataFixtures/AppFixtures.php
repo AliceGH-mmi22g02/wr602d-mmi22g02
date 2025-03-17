1e02044e6de0fe2b3e697832f7f69590a3d4fdc3
Ghantfasia <aliceghanimi@hotmail.com>
2025-03-17 12:53:51 +0100

    feat:customquery

--- application/src/DataFixtures/AppFixtures.php
+++ application/src/DataFixtures/AppFixtures.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\DataFixtures;
 
 use App\Entity\File;
@@ -7,14 +8,20 @@ use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    private $faker;
+
+    // Injection de Faker dans le constructeur
+    public function __construct(Generator $faker)
+    {
+        $this->faker = $faker;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create(); // Instanciation manuelle de Faker
-
         // Crée 3 abonnements liés à la génération et la conversion de PDFs
         $subscriptions = [
             [
@@ -22,27 +29,27 @@ class AppFixtures extends Fixture
                 'description' => 'Accès limité à 1 PDFs par mois.',
                 'maxPdf' => 1,
                 'price' => 0.00,
-                'specialPrice' => 0.00, // Pas de prix spécial, on met le prix standard
-                'specialPriceFrom' => new DateTimeImmutable(), // Date actuelle
-                'specialPriceTo' => (new DateTimeImmutable())->modify('+30 days'), // 30 jours à partir de maintenant
+                'specialPrice' => 0.00,
+                'specialPriceFrom' => new DateTimeImmutable(),
+                'specialPriceTo' => (new DateTimeImmutable())->modify('+30 days'),
             ],
             [
                 'name' => 'Basic',
                 'description' => 'Accès limité à 3 PDFs par mois. Conversion basique.',
                 'maxPdf' => 3,
                 'price' => 1.99,
-                'specialPrice' => 0.99, // Pas de prix spécial, on met le prix standard
-                'specialPriceFrom' => new DateTimeImmutable(), // Date actuelle
-                'specialPriceTo' => (new DateTimeImmutable())->modify('+30 days'), // 30 jours à partir de maintenant
+                'specialPrice' => 0.99,
+                'specialPriceFrom' => new DateTimeImmutable(),
+                'specialPriceTo' => (new DateTimeImmutable())->modify('+30 days'),
             ],
             [
                 'name' => 'Premium',
                 'description' => 'Conversion de 6 PDFs par mois et des options avancées.',
                 'maxPdf' => 6,
                 'price' => 2.99,
-                'specialPrice' => 1.99, // Prix spécial
-                'specialPriceFrom' => new DateTimeImmutable(), // Date actuelle
-                'specialPriceTo' => (new DateTimeImmutable())->modify('+30 days'), // 30 jours à partir de maintenant
+                'specialPrice' => 1.99,
+                'specialPriceFrom' => new DateTimeImmutable(),
+                'specialPriceTo' => (new DateTimeImmutable())->modify('+30 days'),
             ],
             [
                 'name' => 'Pro',
@@ -50,9 +57,9 @@ class AppFixtures extends Fixture
                 avec des fonctionnalités personnalisées.',
                 'maxPdf' => 9,
                 'price' => 3.99,
-                'specialPrice' => 2.99, // Pas de prix spécial
-                'specialPriceFrom' => new DateTimeImmutable(), // Date actuelle
-                'specialPriceTo' => (new DateTimeImmutable())->modify('+30 days'), // 30 jours à partir de maintenant
+                'specialPrice' => 2.99,
+                'specialPriceFrom' => new DateTimeImmutable(),
+                'specialPriceTo' => (new DateTimeImmutable())->modify('+30 days'),
             ],
         ];
 
@@ -70,9 +77,9 @@ class AppFixtures extends Fixture
             // Crée des utilisateurs pour chaque abonnement
             for ($i = 1; $i <= 3; $i++) {
                 $user = new User();
-                $user->setEmail($faker->email);
-                $user->setLastname($faker->lastName);
-                $user->setFirstname($faker->firstName);
+                $user->setEmail($this->faker->email);
+                $user->setLastname($this->faker->lastName);
+                $user->setFirstname($this->faker->firstName);
                 $user->setPassword(password_hash('password', PASSWORD_BCRYPT)); // Ajoute un mot de passe chiffré
                 $user->setRoles(['ROLE_USER']);
                 $user->setSubscription($subscription);
@@ -83,13 +90,13 @@ class AppFixtures extends Fixture
                 $numFiles = rand(1, 3); // Chaque utilisateur aura entre 1 et 3 fichiers
                 for ($j = 0; $j < $numFiles; $j++) {
                     $file = new File();
-                    $file->setName($faker->word . '.pdf');
-                    $file->setCreatedAt((new DateTimeImmutable())->modify('now')); // Utilisation de \DateTimeImmutable
+                    $file->setName($this->faker->word . '.pdf');
+                    $file->setCreatedAt((new DateTimeImmutable())->modify('now'));
 
-                    // Lier le fichier à l'utilisateur via l'ID de l'utilisateur
-                    $file->setUser($user); // Cela associe le fichier à l'utilisateur
+                    // Lier le fichier à l'utilisateur
+                    $file->setUser($user);
 
-                    $manager->persist($file); // Sauvegarde le fichier
+                    $manager->persist($file);
                 }
             }
         }
